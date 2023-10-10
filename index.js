0bc7a3fe71f0856898ff71bf9402756298115195
Adrian <mahdigh@live.com>
2023-10-10 12:52:51 +0330

    exercise 3.3 done. added single entry display for phonebook along with 404 error handling for non-existent entries.

--- index.js
+++ index.js
@@ -34,6 +34,18 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+  
+})
+
 app.get("/info", (request, response) => {
   let startTime = new Date();
   response.send(
