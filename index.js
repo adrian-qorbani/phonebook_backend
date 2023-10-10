7cc7e7b21e9c0caa0eeb40c656f2beb803c773d5
Adrian <mahdigh@live.com>
2023-10-10 12:36:36 +0330

    exercise 3.1 done. nodemon installed as dev dependency.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+// data
 let persons = [
   {
     id: 1,
@@ -24,6 +25,7 @@ let persons = [
   },
 ];
 
+// server routes
 app.get('/', (request, response) => {
   response.send('<h1>Hello Server!</h1>')
 })
@@ -32,6 +34,7 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+// port and 
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
