c4519ac3632393955c32e31fbcb11a50a757927a
Adrian <mahdigh@live.com>
2023-10-12 06:55:59 +0330

    added frontend dist.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(morgan(':method :url :body'))
 app.use(cors());
+app.use(express.static('dist'))
 
 morgan.token('body', req => {
   return JSON.stringify(req.body)
